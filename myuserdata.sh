8ad52af991e930c13bd7c0b1ac68fb8c4e00f59e
Dhaval Pandya <pandya.d@northeastern.edu>
2020-11-03 23:56:47 -0500

    update user data script

--- myuserdata.sh
+++ myuserdata.sh
@@ -7,7 +7,7 @@ echo "export PASSWORD=${dbpassword}" >> setenv.sh
 echo "export DBUSERNAME=${dbusername}" >> setenv.sh 
 echo "export region='${awsregion}'" >> setenv.sh
 echo "export bucketName='${bucketname}'" >> setenv.sh 
-echo "export JAVA_OPTS=\"\$JAVA_OPTS -Dspring.datasource.url='jdbc:mysql://${dbhostname}/csye6225?createDatabaseIfNotExist=true&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC' -Djava.io.tmpdir='/opt/tomcat/temp' -Dspring.servlet.multipart.location='/opt/tomcat/temp' -Dspring.datasource.username=${dbusername} -Dspring.datasource.password=${dbpassword}\"" >> setenv.sh
+echo "export JAVA_OPTS=\"\$JAVA_OPTS -Dspring.datasource.url='${connectionStringName}' -Djava.io.tmpdir='/opt/tomcat/temp' -Dspring.servlet.multipart.location='/opt/tomcat/temp' -Dspring.datasource.username=${dbusername} -Dspring.datasource.password=${dbpassword}\"" >> setenv.sh
 
 
 
